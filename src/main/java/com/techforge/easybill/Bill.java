aebb0c96586bfb9219ba733caa94e399d1690ee3
Inbanithi <inbanithi107@gmail.com>
2024-10-17 19:11:10 +0530

    bill added

--- src/main/java/com/techforge/easybill/Bill.java
+++ src/main/java/com/techforge/easybill/Bill.java
@@ -7,45 +7,45 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
 public class Bill {
 
     @FXML
-    private TableView<Product> tableView;
+     private TextField productid;
 
     @FXML
-    private TableColumn<Product, Integer> tableid;
+    private TextField productname;
 
     @FXML
-    private TableColumn<Product, String> tableitem;
+    private TextField productrate;
 
     @FXML
-    private TableColumn<Product, Integer> tableqty;
+    private TextField productmrp;
 
     @FXML
-    private TableColumn<Product, Integer> tableprice;
+    private TextField productqty;
 
     @FXML
-    private TableColumn<Product, Integer> tablerate;
+    private TextField totalrate;
 
-    public void initialize(){
-        tableid.setCellValueFactory(new PropertyValueFactory<>("id"));
+    public void handleenterkey(KeyEvent keyEvent){
+        if (keyEvent.getCode()== KeyCode.ENTER){
+            handlekeyenter();
+        }
+    }
 
-        tableid.setCellValueFactory(new PropertyValueFactory<>("name"));
+    public void handlekeyenter(){
+        if (productid.getText().isEmpty()){
 
-        tableid.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        }
 
-        tableid.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-        tableid.setCellValueFactory(new PropertyValueFactory<>("rate"));
 
-        ObservableList<Product> products = FXCollections.observableArrayList(
-                new Product(1, "Apple", "25", 50),
-                new Product(2, "Biscuit", "250", 80),
-                new Product(3, "Icecream", "205", 100)
-        );
-        tableView.setItems(products);
     }
 
 }
